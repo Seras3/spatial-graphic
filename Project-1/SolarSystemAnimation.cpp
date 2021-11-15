454b5bdd7f479a705b4cfcc9879850248e263e95
Diana Poinarita <61639111+PoinaritaDiana@users.noreply.github.com>
2021-11-15 12:56:39 +0200

    Add moon rotation

--- Project-1/SolarSystemAnimation.cpp
+++ Project-1/SolarSystemAnimation.cpp
@@ -34,7 +34,8 @@ const int N = 360;
 const int R = 50;
 const float PI = 3.141516;
 glm::mat4 myMatrix, resizeMatrix, scaleMatrix, translMatrix, rotateMatrix, 
-translPlayerMatrix, rotatePlayerMatrix, rotateVerticalPlayerMatrix, launchPlayerMatrix;
+	translMoonMatrix1, translMoonMatrix2, rotateMoonMatrix,
+	translPlayerMatrix, rotatePlayerMatrix, rotateVerticalPlayerMatrix, launchPlayerMatrix;
 
 glm::mat4 view, projection;
 
@@ -42,13 +43,11 @@ float Obsx = 0.0f, Obsy = 0.0f, Obsz = 800.f;
 float Refx = 0.0f, Refy = 0.0f;
 float width = 1200, height = 600, fov = 90, znear = 1, zfar = 1000;
 
-
 bool PLAYER_LAUNCHED = false;
 bool PLAYER_SHOULD_INIT_MATRIX = false;
 
-float i = 0.0;
-float angle = 0.0;
-
+float planetAngle = 0.0;
+float moonAngle = 0.0;
 float planetScaleRaport[] = { 0.25, 0.5, 0.55, 0.3, 0.75, 0.70, 0.60, 0.60 };
 
 
@@ -96,16 +95,15 @@ void CreateVBO(void)
 		0.0f, 1.0f, 0.0f, 1.0f,
 		0.0f, 0.0f, 1.0f, 1.0f,
 		1.0f, 0.0f, 0.0f, 1.0f,
-
 		1.0f, 1.0f, 0.0f, 1.0f,
 	};
 
 	static const GLfloat Textures[] =
 	{
 		0.0f, 0.0f, // st jos 
-		0.0f, 1.0f, // dr jos
+		1.0f, 0.0f, // dr jos
 		1.0f, 1.0f, // dr sus
-		1.0f, 0.0f, // st sus
+		0.0f, 1.0f, // st sus
 	};
 
 	glGenBuffers(1, &VboId);
@@ -147,7 +145,6 @@ void DestroyVBO(void)
 
 void LoadTexture(void)
 {
-	
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
 
@@ -163,7 +160,6 @@ void LoadTexture(void)
 
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
-
 }
 
 void CreateShaders(void)
@@ -181,8 +177,8 @@ void Initialize(void)
 {
 	glClearColor(0.0f, 0.0f, 0.8f, 0.0f);
 	CreateShaders();
-	LoadTexture();
 
+	LoadTexture();
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glUniform1i(glGetUniformLocation(ProgramId, "myTexture"), 0);
@@ -213,10 +209,10 @@ void setTexCode(int cod)
 }
 
 
-
 void rotatePlanetsAroundSun(int n)
 {
-	angle = angle + 0.1;
+	moonAngle = moonAngle +0.35;
+	planetAngle = planetAngle + 0.1;
 	glutPostRedisplay();
 	glutTimerFunc(50, rotatePlanetsAroundSun, 0);
 }
@@ -232,7 +228,6 @@ void mouse(int button, int state, int x, int y)
 		break;
 	}
 }
-
 
 void processNormalKeys(unsigned char key, int x, int y) {
 
@@ -272,26 +267,36 @@ void processSpecialKeys(int key, int x, int y) {
 	}
 }
 
-
-
 void drawOrbit(int p) {
 	scaleMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(2.f * p, 2.f * p, 1.0));
 	setMyMatrix(scaleMatrix);
 	glDrawArrays(GL_LINE_LOOP, PlayerVCount, N);
 }
 
+void drawMoon() {
+	rotateMoonMatrix = glm::rotate(glm::mat4(1.0f), moonAngle, glm::vec3(0.0, 0.0, 1.0));
+	scaleMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(0.1, 0.1, 1.0));
+	translMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(7 * R, 0.0, 0.0));
+	translMoonMatrix1 = glm::translate(glm::mat4(1.0f), glm::vec3(-6 * R, 0.0, 0.0));
+	translMoonMatrix2 = glm::translate(glm::mat4(1.0f), glm::vec3(6* R, 0.0, 0.0));
+
+	setMyMatrix(rotateMatrix * translMoonMatrix2 * rotateMoonMatrix * translMoonMatrix1 * translMatrix * scaleMatrix);
+	glDrawArrays(GL_TRIANGLE_FAN, PlayerVCount, N);
+}
+
 void drawPlanet(int p, float scaleRaport) {
 	scaleMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(scaleRaport, scaleRaport, 1.0));
 	translMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(2 * p * R, 0.0, 0.0));
+	setMyMatrix(rotateMatrix * translMatrix * scaleMatrix);
+	glDrawArrays(GL_TRIANGLE_FAN, PlayerVCount, N);
 	if (p == 3) {
 		if (!PLAYER_LAUNCHED) {
 			translPlayerMatrix = translMatrix;
 			Obsx = (rotateMatrix * translMatrix)[3][0];
 			Obsy = (rotateMatrix * translMatrix)[3][1];
 		}
+		drawMoon();
 	}
-	setMyMatrix(rotateMatrix * translMatrix * scaleMatrix);
-	glDrawArrays(GL_TRIANGLE_FAN, PlayerVCount, N);
 }
 
 void RenderFunction(void)
@@ -320,13 +325,11 @@ void RenderFunction(void)
 	setMyMatrix(resizeMatrix);
 	glDrawArrays(GL_TRIANGLE_FAN, PlayerVCount, N);
 
-	
-	
 	for (int p = 1; p <= 8; p++) {
 		drawOrbit(p);
-		rotateMatrix = glm::rotate(glm::mat4(1.0f), angle/p, glm::vec3(0.0, 0.0, 1.0));
+		rotateMatrix = glm::rotate(glm::mat4(1.0f), planetAngle/p, glm::vec3(0.0, 0.0, 1.0));
 		if (p == 3 && PLAYER_SHOULD_INIT_MATRIX) {
-			rotateVerticalPlayerMatrix = glm::rotate(glm::mat4(1.0f), -angle / p, glm::vec3(0.0, 0.0, 1.0));
+			rotateVerticalPlayerMatrix = glm::rotate(glm::mat4(1.0f), -planetAngle / p, glm::vec3(0.0, 0.0, 1.0));
 			rotatePlayerMatrix = rotateMatrix;
 		}
 		drawPlanet(p, planetScaleRaport[p - 1]);
@@ -343,10 +346,8 @@ void RenderFunction(void)
 		glDrawArrays(GL_TRIANGLE_FAN, 0, PlayerVCount);
 	}
 	
-
 	glFlush();
 }
-
 
 
 int main(int argc, char* argv[])
